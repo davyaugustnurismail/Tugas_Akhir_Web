f68152abf32c3753bd6ecd4071a311adbd5d7dd4
AgungPajar <presetpjr@gmail.com>
2025-02-03 13:32:09 +0700

    Migrate

---
+++ database/migrations/2025_02_03_062559_create_blanknote_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blanknote', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('desc');
+            $table->date('tanggal');
+            $table->integer('tanda');
+            $table->enum( 'priority', ['default','very_low', 'low', 'medium', 'high', 'very_high']);
+            $table->boolean('shared');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blanknote');
+    }
+};
